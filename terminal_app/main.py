4b5ca681cd2704dd3ba2bedbbab0293a08efaeca
MonsieurCo <monsieur.cousson@gmail.com>
2022-11-27 15:50:29 +0100

    pin auth finished

--- terminal_app/main.py
+++ terminal_app/main.py
@@ -26,40 +26,57 @@ cardrequest = CardRequest(timeout=1, cardType=cardtype)
 cardservice = cardrequest.waitforcard()
 
 cardservice.connection.connect()
-observer = ConsoleCardConnectionObserver()
-cardservice.connection.addObserver(observer)
+# observer = ConsoleCardConnectionObserver()
+# cardservice.connection.addObserver(observer)
 
 APPLET_AID = [0xA0, 0x00, 0x00, 0x00, 0x62, 0x03, 0x01, 0x0C, 0x06, 0x01, 0x02]
 APDU_PIN = [APPLET, VERIFY, 0x00, 0x00, 0x04, 0x00, 0x01, 0x02, 0x03]
 APDU_SELECT = [0x00, 0xA4, 0x04, 0x00, len(APPLET_AID)] + APPLET_AID
 APDU_HELLO = [APPLET, 0x00, 0x00, 0x00, 0x05]
-
+""" 
 response, sw1, sw2 = cardservice.connection.transmit(APDU_SELECT)
 response, sw1, sw2 = cardservice.connection.transmit(APDU_PIN)
 response, sw1, sw2 = cardservice.connection.transmit(APDU_HELLO)
-
+ """
 
 
 class SmartCard:
-    def __init__(self,debug=False):
+    def __init__(self,connection,debug=False):
         self.AID = APPLET_AID
+        self.APDU_SELECT = [0x00, 0xA4, 0x04, 0x00, len(self.AID)] + self.AID
         self.connection = None
         self.PIN = None
         self.cardtype = AnyCardType()
         self.observer =ConsoleCardConnectionObserver()
-
+        self.connection = connection
+        self.verified = False
+        self._connect()
 
     def _connect(self):
-        cardrequest = CardRequest(timeout=1, cardType=self.cardtype)
-        cardservice = cardrequest.waitforcard()
-        cardservice.connection.connect()
-        self.connection = cardservice.connection
+        # cardrequest = CardRequest(timeout=1, cardType=self.cardtype)
+        # cardservice = cardrequest.waitforcard()observer = ConsoleCardConnectionObserver()
+# cardservice.connection.addObserver(observer)
+        # cardservice.connection.connect()
+        # self.connection = cardservice.connection
         if debug :  
             self.connection.addObserver(self.observer)
-        self.PIN = PIN(self.connection)
+        self.PIN = PIN(self.connection,self.AID,self.APDU_SELECT)
+        
     
+    def verify(self):
+        return self.PIN.inputPin()
 
+    def send_apdu(self,apdu):
+        response, sw1, sw2 = self.connection.transmit(apdu)
+        return response, sw1, sw2
+
+    def select_applet(self):
+        return self.send_apdu(self.APDU_SELECT)
+    def hello(self):
+        return self.send_apdu(APDU_HELLO)
 
 
 if __name__ == "__main__":
-    card = SmartCard()
+    card = SmartCard(cardservice.connection,debug=True)
+    card.verify()
+    card.hello()
--- terminal_app/src/pin.py
+++ terminal_app/src/pin.py
@@ -2,19 +2,42 @@ class PINVerification:
     pass
 
 class PIN:
-    def __init__(self,connection,AID):
+    def __init__(self,connection,AID,APDU_SELECT):
         self.tries_left = 3
         self.AID = AID
+        self.APDU_SELECT = APDU_SELECT
         self.CLA = 0x80
         self.INS_AUTH = 0x20
         self.INS_ACTIVATE = 0x01
         self.connection = connection
+        print(self.connection)
 
     def verify(self, pin):
+        try:
+            pin = [int(x) for x in pin]
+        except:
+            print("PIN must be numeric")
+            return False
         adpu = [self.CLA, self.INS_AUTH, 0x00, 0x00, len(pin)] + pin
+        response, sw1 , sw2 = self.connection.transmit(self.APDU_SELECT)
         data, sw1, sw2 = self.connection.transmit(adpu)
         if sw1 == 0x90 and sw2 == 0x00:
             return True
         else:
             self.tries_left -= 1
             return False
+
+    def inputPin(self):
+        verify = False
+        while not verify:
+            pin = input("Enter PIN: ")
+            if len(pin) < 4 or len(pin) > 8:
+                print("PIN must be between 4 and 8 digits")
+                continue
+            verify = self.verify(pin)
+            if not verify:
+                print("Wrong PIN, %d tries left" % self.tries_left)
+            if self.tries_left == 0:
+                print ("No more tries left, card blocked")
+                return False
+        return verify
