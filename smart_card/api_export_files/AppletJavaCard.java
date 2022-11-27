c49e3ab0bdebca3b02fcf4cbdeba9e885a01328d
takitsu21 <dylannbatisse@gmail.com>
2022-11-27 15:45:46 +0100

    rsa 512

--- smart_card/api_export_files/AppletJavaCard.java
+++ smart_card/api_export_files/AppletJavaCard.java
@@ -27,9 +27,11 @@
 // Original author:  Mitch Butler
 // */
 
-package smart_card;
+package api_export_files.javacard;
 
 import javacard.framework.*;
+import javacard.security.*;
+import javacardx.crypto.Cipher;
 
 /**
  */
@@ -57,6 +59,17 @@ public class AppletJavaCard extends Applet {
     OwnerPIN pin;
     final static byte VERIFY = (byte) 0x20;
 
+    final static byte INS_GET_PUBLIC_KEY = (byte) 0x01;
+
+    final static RSAKeyPair rsaKeyPair = new RSAKeyPair();
+    private final Cipher cipherRSA;
+    RSAPrivateCrtKey rsa_PrivateCrtKey;
+    RSAPublicKey rsa_PublicKey;
+    KeyPair rsa_KeyPair;
+
+    final static byte GETPUBLICKEYMod_ = (byte) 0x01;
+    final static byte GETPUBLICKEYExp_ = (byte) 0x02;
+
     protected AppletJavaCard(byte[] bArray, short bOffset, byte bLength) {
         echoBytes = new byte[LENGTH_ECHO_BYTES];
         pin = new OwnerPIN(PIN_TRY_LIMIT, MAX_PIN_SIZE);
@@ -64,7 +77,32 @@ public class AppletJavaCard extends Applet {
         // The installation parameters contain the PIN
         // initialization value
         pin.update(new byte[] { 0x00, 0x01, 0x02, 0x03 }, (short) 0, (byte) 4);
+        rsa_KeyPair = new KeyPair(KeyPair.ALG_RSA_CRT, KeyBuilder.LENGTH_RSA_1024);
+        rsa_KeyPair.genKeyPair();
+        rsa_PublicKey = (RSAPublicKey) rsa_KeyPair.getPublic();
+        rsa_PrivateCrtKey = (RSAPrivateCrtKey) rsa_KeyPair.getPrivate();
+
+        cipherRSA = Cipher.getInstance(Cipher.ALG_RSA_PKCS1, false);
         register();
+    }
+
+    private void getPublicKeyMod( APDU apdu )
+    {
+        byte[] buffer = apdu.getBuffer();
+        rsa_PublicKey.getModulus(buffer, ISO7816.OFFSET_CDATA);
+        apdu.setOutgoing();
+        apdu.setOutgoingLength((short) 128);
+        apdu.sendBytesLong(buffer, ISO7816.OFFSET_CDATA, (short) 128);
+    }
+
+    private void getPublicKeyExp( APDU apdu )
+    {
+        byte[] buffer = apdu.getBuffer();
+        rsa_PublicKey.getExponent(buffer, ISO7816.OFFSET_CDATA);
+        apdu.setOutgoing();
+        apdu.setOutgoingLength((short) 4);
+        apdu.sendBytesLong(buffer, (short) ISO7816.OFFSET_CDATA, (short) 4);
+
     }
 
     /**
@@ -96,6 +134,48 @@ public class AppletJavaCard extends Applet {
 
     }// end of select method
 
+    private void computeSignature(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        short length = (short) (buffer[ISO7816.OFFSET_LC] & 0x00FF);
+        // Make sure that DigestInfo is <= 40% of the RSA key length.
+//        if ((short) (length * 4) > (short) (KeyBuilder.LENGTH_RSA_512 * 10) ||
+//            apdu.setIncomingAndReceive() != length) {
+//          ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+//        }
+//        if (!pinSubmitted[PIN_INDEX_PW1] || !pins[PIN_INDEX_PW1].isValidated()) {
+//          ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
+//        }
+//        if (!signatureKey.getPrivate().isInitialized()) {
+//          ISOException.throwIt(ISO7816.SW_FILE_NOT_FOUND);
+//        }
+//        if (pinValidForMultipleSignatures == (byte) 0) {
+//          pinSubmitted[PIN_INDEX_PW1] = false;
+//        }
+
+        cipherRSA.init(rsaKeyPair.getPrivate(), Cipher.MODE_ENCRYPT);
+        cipherRSA.doFinal(buffer, ISO7816.OFFSET_CDATA, length, buffer, (short) 0);
+        JCSystem.beginTransaction();
+//        if (signatureCounter[2] != (byte) 0xFF) {
+//          signatureCounter[2] = (byte) ((signatureCounter[2] & 0xFF) + 1);
+//        } else {
+//          signatureCounter[2] = 0;
+//          if (signatureCounter[1] != (byte) 0xFF) {
+//            signatureCounter[1] = (byte) ((signatureCounter[1] & 0xFF) + 1);
+//          } else if (signatureCounter[0] != (byte) 0xFF) {
+//            signatureCounter[1] = 0;
+//            signatureCounter[0] = (byte) ((signatureCounter[0] & 0xFF) + 1);
+//          } else {
+//            JCSystem.abortTransaction();
+//            ISOException.throwIt(ISO7816.SW_FILE_FULL);
+//          }
+//        }
+        JCSystem.commitTransaction();
+        apdu.setOutgoingAndSend((short) 0, KeyBuilder.LENGTH_RSA_512);
+      }
+
+    public void sendPublicKey(APDU apdu) {
+
+    }
     private void getHelloWorld(APDU apdu) {
         if (!pin.isValidated())
             ISOException.throwIt(SW_PIN_VERIFICATION_REQUIRED);
@@ -139,13 +219,21 @@ public class AppletJavaCard extends Applet {
         if (CLA != HW_CLA) {
             ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
         }
-
-        if (INS == INS_HELLO) {
-            getHelloWorld(apdu);
-        } else if (INS == VERIFY) {
-            verify(apdu);
-        } else {
-            ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+        switch (INS) {
+            case INS_HELLO:
+                getHelloWorld(apdu);
+                break;
+            case VERIFY:
+                verify(apdu);
+                break;
+            case GETPUBLICKEYMod_:
+                getPublicKeyMod(apdu);
+                break;
+            case GETPUBLICKEYExp_:
+                getPublicKeyExp(apdu);
+                break;
+            default:
+                ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
         }
     }
 
